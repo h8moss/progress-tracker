dea20d8dfd591ca695fb5dc90243d56aeb295085
h8m0ss <danarmenta32@gmail.com>
2023-05-22 15:21:25 -0600

    removed unused greet command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,10 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+fn read_file(path: &str) -> String {}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
