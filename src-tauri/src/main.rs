ec32c4fae63d1776dd729d197467459adb223921
h8m0ss <danarmenta32@gmail.com>
2023-05-22 16:19:39 -0600

    FEAT: Added file reading command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs::read_to_string;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn read_file(path: String) -> String {
+    let contents = match read_to_string(path) {
+        Ok(c) => c,
+        Err(_) => String::new(),
+    };
+
+    contents
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![read_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
