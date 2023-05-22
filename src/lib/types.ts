7a3efb4a6b5e68101f8ae8f22d5cdc7ad0f9f5c6
h8m0ss <danarmenta32@gmail.com>
2023-05-22 11:59:13 -0600

    FEAT: Added JSDoc annotations FEAT: Created the `JSONValue` type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,34 @@
+/**
+ Describes some task with an importance ({@link ProgressNode.weight}), 
+ a name ({@link ProgressNode.title}), and optionally, 
+ sub-tasks ({@link ProgressNode.children})
+ */
 export type ProgressNode = {
+  /**
+ @param {string} title The title of the node.
+   */
   title: string;
+  /**
+ @param {boolean?} isDone Weather the task is done or not
+ */
   isDone?: boolean;
+  /**
+ @param {ProgressNode[]?} children A list of subtasks for this node.
+ */
   children?: ProgressNode[];
+  /**
+ @param {number} weight How important this task is.
+ */
   weight: number;
 };
+
+/**
+ Defines an object that can be represented in a .json file
+ */
+export type JSONValue =
+  | string
+  | number
+  | boolean
+  | null
+  | JSONValue[]
+  | { [key: string]: JSONValue };
