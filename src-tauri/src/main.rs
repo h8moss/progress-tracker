82f46dcc3f5a759745da27b354a2e9943bd7daf1
h8m0ss <danarmenta32@gmail.com>
2023-05-23 00:01:17 -0600

    FEAT: Added the `get_video_duration` command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use ffprobe::ffprobe;
 use std::fs::read_to_string;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn read_file(path: String) -> String {
     let contents = match read_to_string(path) {
@@ -14,9 +14,22 @@ fn read_file(path: String) -> String {
     contents
 }
 
+#[tauri::command]
+fn get_video_duration(path: String) -> f32 {
+    let result = match ffprobe(path) {
+        Ok(info) => match info.format.duration {
+            Some(val) => val.parse().unwrap(),
+            None => 0.0,
+        },
+        Err(_err) => 0.0,
+    };
+
+    result
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![read_file])
+        .invoke_handler(tauri::generate_handler![get_video_duration, read_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
