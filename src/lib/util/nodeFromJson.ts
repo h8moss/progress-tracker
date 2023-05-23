f755f900373a27f531a01736a1e8c4ac0abd433f
h8m0ss <danarmenta32@gmail.com>
2023-05-22 18:30:58 -0600

    FIX: Updated to reflect weight being an optional parameter

--- src/lib/util/nodeFromJson.ts
+++ src/lib/util/nodeFromJson.ts
@@ -1,4 +1,5 @@
 import type { JSONValue, ProgressNode } from "../types";
+import makeNodeValid from "./makeNodeValid";
 
 /**
  creates a new {@link ProgressNode} using JSON data
@@ -6,27 +7,34 @@ import type { JSONValue, ProgressNode } from "../types";
  */
 const nodeFromJson = (json: JSONValue): ProgressNode => {
   if (json === null || typeof json !== "object" || Array.isArray(json)) {
-    throw "JSON value does not represent a node";
+    throw "JSON value does not represent an object, and thus, it can't be a node";
   }
   const { title, weight, children, isDone } = json;
 
   if (
     typeof title !== "string" ||
-    typeof weight !== "number" ||
+    (typeof weight !== "undefined" && typeof weight !== "number") ||
     (typeof children !== "undefined" && !Array.isArray(children)) ||
     (typeof isDone !== "undefined" && typeof isDone !== "boolean")
   ) {
-    throw "JSON value does not represent a node";
+    console.error({
+      json,
+      title,
+      weight,
+      children,
+      isDone,
+    });
+    throw "JSON value does not represent a node. Missing either title, weight, children or isDone";
   }
 
-  return {
+  return makeNodeValid({
     title,
     weight,
     children: children
       ? children.map((value) => nodeFromJson(value))
       : undefined,
     isDone,
-  };
+  });
 };
 
 export default nodeFromJson;
