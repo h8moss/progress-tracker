962a29001d7d2dd6136249fef8dde942e0d96c8b
h8m0ss <danarmenta32@gmail.com>
2023-05-22 18:30:15 -0600

    REFAC: Updated util/index.ts

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -1,4 +1,15 @@
 import getTotalWeight from "./getTotalWeight";
 import nodeFromJson from "./nodeFromJson";
+import promiseTimeout from "./promiseTimeout";
+import nodeFromJsonPath from "./nodeFromJsonPath";
+import makeNodeValid from "./makeNodeValid";
+import isNodeValid from "./isNodeValid";
 
-export { getTotalWeight, nodeFromJson };
+export {
+  getTotalWeight,
+  nodeFromJson,
+  promiseTimeout,
+  nodeFromJsonPath,
+  makeNodeValid,
+  isNodeValid,
+};
