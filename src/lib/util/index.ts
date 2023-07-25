5d2a6df8095253e93902fcfffca584f7e75954cc
h8m0ss <danarmenta32@gmail.com>
2023-07-24 23:48:46 -0600

    FEAT: Added `AddRecentData` function

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -1,5 +1,13 @@
 import appEventListener from "./appEventListener";
 import interpretWeight from "./interpretWeight";
 import promiseTimeout from "./promiseTimeout";
+import addRecentData from "./addRecentData";
+import ContextMenuItems from "./ContextMenuItems";
 
-export { appEventListener, interpretWeight, promiseTimeout };
+export {
+  appEventListener,
+  interpretWeight,
+  promiseTimeout,
+  addRecentData,
+  ContextMenuItems,
+};
